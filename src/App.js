2b9163fe2ea50585d9c805fbb2566e8b12ed3f14
Denwer1 <denwer0204@gmail.com>
2024-02-27 19:07:03 +0300

    context

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import Cart from "./pages/Cart";
-import { useState } from "react";
+import { createContext, useState } from "react";
+
+export const SearchContext = createContext("");
 
 function App() {
   const [searchValue, setSearchValue] = useState("");
 
   return (
     <div className="wrapper">
-      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home searchValue={searchValue} />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </div>
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home searchValue={searchValue} />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </div>
+      </SearchContext.Provider>
     </div>
   );
 }
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./Search.module.scss";
 import SearchIcon from "../../assets/img/search.svg";
 import CrossIcon from "../../assets/img/cross.svg";
+import { SearchContext } from '../../App';
 
-const Search = ({ searchValue, setSearchValue }) => {
+const Search = () => {
+  const {searchValue, setSearchValue} = useContext(SearchContext);
+
   return (
     <div className={styles.root}>
       <img src={SearchIcon} alt="search" className={styles.icon} />
