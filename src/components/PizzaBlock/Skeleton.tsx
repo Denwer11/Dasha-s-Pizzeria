428497514916e7c74d17db3220f436784ec61b11
Denwer1 <denwer0204@gmail.com>
2024-03-10 22:32:18 +0300

    skeleton edits

--- src/components/PizzaBlock/Skeleton.tsx
+++ src/components/PizzaBlock/Skeleton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ContentLoader from "react-content-loader";
 
-const Skeleton: React.FC = (props) => (
+const Skeleton = () => (
   <ContentLoader
     className="pizza-block"
     speed={2}
@@ -10,7 +10,6 @@ const Skeleton: React.FC = (props) => (
     viewBox="0 0 280 500"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
-    {...props}
   >
     <circle cx="135" cy="135" r="125" />
     <rect x="0" y="280" rx="10" ry="10" width="280" height="25" />
